9df51e0c10cdc451dc36103135e45a4e9faf2108
Mab <mabchhan@gmail.com>
2023-03-24 21:48:05 -0700

    validation email

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,79 @@
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
+
 function Contact() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log("Submit Form", formState);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log("Handle Form", formState);
+    }
+  };
   return (
     <section className="d-flex justify-content-center">
-      <form id="contact-form" className="">
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label For="name">Name:</label>
-          <input type="text" name="name" />
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
         </div>
         <div className="mb-3">
-          <label For="email">Email address:</label>
-          <input type="email" name="email" />
-        </div>
-        <div className="mb-3">
-          <label For="message">Message:</label>
-          <textarea name="message" rows="6" />
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
         </div>
 
+        <div className="mb-3">
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            rows="6"
+            defaultValue={message}
+            onBlur={handleChange}
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="text-danger">{errorMessage}</p>
+          </div>
+        )}
         <button type="submit" className="mb-3">
           Submit
         </button>
